bf8bb9a0ae2b583dd72f8dea0283be952ffa6e2b
ilhwan <poj0722@naver.com>
2022-10-09 22:12:35 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/com/pppp0722/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pppp0722.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
